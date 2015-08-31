7e6f50c36bb41c262f2d06d2619c47ce32ce30a9
Dave Jagoda <davejagoda@gmail.com>
2015-08-31 14:34:53 -0700

    add the ability to print out the cells

--- lsSpreadsheet.py
+++ lsSpreadsheet.py
@@ -17,25 +17,34 @@ def get_access_token(tokenFile, refresh=False):
         credentials.authorize(http)
     return(credentials.access_token)
 
-def enumerate_documents(ss_feed, worksheets):
+def enumerate_rows(spreadsheet_id, worksheet_id):
+    cells_feed = gd_client.GetCellsFeed(spreadsheet_id, worksheet_id)
+    print('number of cells found is {}'.format(len(cells_feed.entry)))
+    for entry in cells_feed.entry:
+        print('row:{} col:{} contents:{}'.format(entry.cell.row, entry.cell.col, entry.content.text))
+
+def enumerate_worksheets(spreadsheet_id, show_rows):
+    ws_feed = gd_client.GetWorksheetsFeed(spreadsheet_id)
+    print('number of worksheets found is {}'.format(len(ws_feed.entry)))
+    for entry in ws_feed.entry:
+        worksheet_id = entry.id.text.rsplit('/',1)[1]
+        print(' {} {} rows:{} columns:{}'.format(worksheet_id, entry.title.text, entry.row_count.text, entry.col_count.text))
+        if show_rows:
+            enumerate_rows(spreadsheet_id, worksheet_id)
+
+def enumerate_documents(ss_feed, show_worksheets, show_rows):
     for entry in ss_feed.entry:
         spreadsheet_id = entry.id.text.rsplit('/',1)[1]
         print(spreadsheet_id)
-        if worksheets:
-            enumerate_worksheets(spreadsheet_id)
-
-def enumerate_worksheets(spreadsheet_id):
-    ws_feed = gd_client.GetWorksheetsFeed(spreadsheet_id)
-    print('number of worksheets found with is {}'.format(len(ws_feed.entry)))
-    for entry in ws_feed.entry:
-        worksheet_id = entry.id.text.rsplit('/',1)[1]
-        print(' ' + worksheet_id + ' ' + entry.title.text)
+        if show_worksheets:
+            enumerate_worksheets(spreadsheet_id, show_rows)
 
 if '__main__' == __name__:
     parser = argparse.ArgumentParser()
     parser.add_argument('-t', '--tokenFile', action='store', required=True, help='file containing OAuth token in JSON format')
     parser.add_argument('-n', '--name', action='store', required=True, help='name of the spreadsheet')
     parser.add_argument('-w', '--worksheets', action='store_true', help='set this to list out worksheets (tabs) in each spreadsheet')
+    parser.add_argument('-r', '--rows', action='store_true', help='set this to list out rows in each spreadsheet')
     args = parser.parse_args()
 
     q = gdata.spreadsheet.service.DocumentQuery()
@@ -52,4 +61,4 @@ if '__main__' == __name__:
         gd_client.additional_headers={'Authorization' : 'Bearer %s' % access_token}
         ss_feed = gd_client.GetSpreadsheetsFeed(query=q)
     print('number of spreadsheets found with the name {} is {}'.format(args.name, len(ss_feed.entry)))
-    enumerate_documents(ss_feed, args.worksheets)
+    enumerate_documents(ss_feed, args.worksheets, args.rows)
