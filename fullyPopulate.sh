aa08ca13465463b9e4fdbf488e282247bc1c2fa8
Dave Jagoda <davejagoda@users.noreply.github.com>
2016-03-02 12:45:51 -0800

    add fullyPopulate.sh

---
+++ fullyPopulate.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+for c in {a..z}
+do
+    for r in `seq 1 1000`
+    do
+        echo $c$r
+        ./touchCell.py -t visicalc_token.json -n fullyPopulate -w Sheet1 -c $c$r -s $c$r -f
+    done
+done
