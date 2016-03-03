e883af55a5568593e969f6ac3a370b2558f584a3
Dave Jagoda <davejagoda@users.noreply.github.com>
2016-03-03 08:02:13 -0800

    refactor, handle ISO dates, handle reverse chronological dates

--- validateSpreadsheet.py
+++ validateSpreadsheet.py
@@ -22,10 +22,9 @@ def get_access_token(tokenFile, refresh=False, verbose=False):
         credentials.authorize(http)
     return(credentials.access_token)
 
-def find_blank_columns(spreadsheet_id, worksheet_id, verbose=False):
+def find_blank_columns(cells_feed, verbose=False):
     col_hash = {}
     blank_cols = []
-    cells_feed = gd_client.GetCellsFeed(spreadsheet_id, worksheet_id)
     for entry in cells_feed.entry:
         if verbose: print('row:{} col:{} contents:{}'.format(entry.cell.row, entry.cell.col, entry.content.text))
         col_hash[int(entry.cell.col)] = 1
@@ -35,9 +34,8 @@ def find_blank_columns(spreadsheet_id, worksheet_id, verbose=False):
             blank_cols.append(chr(64 + i))
     print('blank columns:{} max column:{}'.format(','.join(blank_cols), chr(64 + max_col)))
 
-def validate_sheet_for_integers(spreadsheet_id, worksheet_id, verbose=False):
+def validate_sheet_for_integers(cells_feed, verbose=False):
     value = 0
-    cells_feed = gd_client.GetCellsFeed(spreadsheet_id, worksheet_id)
     for entry in cells_feed.entry:
         if verbose: print('row:{} col:{} contents:{}'.format(entry.cell.row, entry.cell.col, entry.content.text))
         if '1' == entry.cell.col:
@@ -49,31 +47,56 @@ def validate_sheet_for_integers(spreadsheet_id, worksheet_id, verbose=False):
                         print('skipped one: {} {}?'.format(value, int(entry.content.text)))
                     value = int(entry.content.text)
 
-def validate_sheet_for_dates(spreadsheet_id, worksheet_id, verbose=False):
-    date = None
-    cells_feed = gd_client.GetCellsFeed(spreadsheet_id, worksheet_id)
+def dates_ok(date1, date2, reverse=False):
+    if date1 == date2: # they are the same, thus not out of order
+        return(True)
+    if date1 < date2: # they are in ascending order
+        return(not reverse)
+    else: # they are in descending order
+        return(reverse)
+
+def validate_sheet_for_dates(cells_feed, reverse=False, verbose=False):
+    compare_date = None
     for entry in cells_feed.entry:
         if verbose: print('row:{} col:{} contents:{}'.format(entry.cell.row, entry.cell.col, entry.content.text))
         if '1' == entry.cell.col:
-            if ('Date' == entry.content.text):
-                pass
+            if (None == entry.content.text):
+                print('None cell')
+                continue
+            if ('' == entry.content.text):
+                print('blank cell')
+                continue
+            if verbose: print(type(entry.content.text), len(entry.content.text), entry.content.text)
+            if ('date' == entry.content.text.lower()):
+                print('date header')
+                continue
             else:
-                if verbose: print(entry.content.text)
+                if verbose: print('raw entry.content.text:{}'.format(entry.content.text))
                 try:
-                    if verbose: print(datetime.datetime.strptime(entry.content.text, '%m/%d/%Y'))
-                    if None == date:
-                        date = datetime.datetime.strptime(entry.content.text, '%m/%d/%Y')
+                    if None == compare_date:
+                        try:
+                            datefmt = '%Y-%m-%d'
+                            compare_date = datetime.datetime.strptime(entry.content.text, datefmt)
+                            print('ISO date')
+                        except:
+                            try:
+                                datefmt = '%m/%d/%Y'
+                                compare_date = datetime.datetime.strptime(entry.content.text, datefmt)
+                                print('American date')
+                            except:
+                                datefmt = None
+                                print('could not identify date')
                     else:
-                        if date <= datetime.datetime.strptime(entry.content.text, '%m/%d/%Y'):
-                            date = datetime.datetime.strptime(entry.content.text, '%m/%d/%Y')
+                        if verbose: print(datetime.datetime.strptime(entry.content.text, datefmt))
+                        if dates_ok(compare_date, datetime.datetime.strptime(entry.content.text, datefmt), reverse):
+                            compare_date = datetime.datetime.strptime(entry.content.text, datefmt)
                         else:
                             print('out of order date on worksheet:{} row:{}'.format(worksheet_id, entry.cell.row))
                 except:
                     print('bad date format on worksheet:{} row:{}'.format(worksheet_id, entry.cell.row))
 
-def list_all_worksheets(spreadsheet_id, verbose=False):
+def list_all_worksheets(ws_feed, verbose=False):
     results = []
-    ws_feed = gd_client.GetWorksheetsFeed(spreadsheet_id)
     for entry in ws_feed.entry:
         worksheet_title = entry.title.text
         worksheet_id = entry.id.text.rsplit('/',1)[1]
@@ -98,6 +121,7 @@ if '__main__' == __name__:
     group = parser.add_mutually_exclusive_group(required=True)
     group.add_argument('-b', '--blanks', action='store_true')
     group.add_argument('-d', '--dates', action='store_true')
+    group.add_argument('-r', '--reversedDates', action='store_true')
     group.add_argument('-i', '--integers', action='store_true')
     args = parser.parse_args()
     q = gdata.spreadsheet.service.DocumentQuery()
@@ -114,11 +138,17 @@ if '__main__' == __name__:
         ss_feed = gd_client.GetSpreadsheetsFeed(query=q)
     for spreadsheet_title, spreadsheet_id in list_all_spreadsheets(ss_feed, verbose=args.verbose):
         print('SpreadsheetTitle: {} SpreadsheetID: {}'.format(spreadsheet_title, spreadsheet_id))
-        for worksheet_title, worksheet_id in list_all_worksheets(spreadsheet_id, verbose=args.verbose):
+        ws_feed = gd_client.GetWorksheetsFeed(spreadsheet_id)
+        for worksheet_title, worksheet_id in list_all_worksheets(ws_feed, verbose=args.verbose):
             print('WorksheetTitle: {} WorksheetID: {}'.format(worksheet_title, worksheet_id))
+            q = gdata.spreadsheet.service.CellQuery()
+            q.return_empty = 'false' # may want to change this to find blank rows
+            cells_feed = gd_client.GetCellsFeed(spreadsheet_id, worksheet_id, query=q)
             if args.blanks:
-                find_blank_columns(spreadsheet_id, worksheet_id, verbose=args.verbose)
+                find_blank_columns(cells_feed, verbose=args.verbose)
             if args.dates:
-                validate_sheet_for_dates(spreadsheet_id, worksheet_id, verbose=args.verbose)
+                validate_sheet_for_dates(cells_feed, reverse=False, verbose=args.verbose)
+            if args.reversedDates:
+                validate_sheet_for_dates(cells_feed, reverse=True, verbose=args.verbose)
             if args.integers:
-                validate_sheet_for_integers(spreadsheet_id, worksheet_id, verbose=args.verbose)
+                validate_sheet_for_integers(cells_feed, verbose=args.verbose)
